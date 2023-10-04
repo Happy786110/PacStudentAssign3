796fccf77ced57e5d67d71aeacf0e91ca1d34519
Happy786110 <143800959+Happy786110@users.noreply.github.com>
2023-10-04 22:27:44 +1100

    PacStudent Movement Animation In Progress

--- Assets/Assets/Scripts/PacStudentMovement.cs
+++ Assets/Assets/Scripts/PacStudentMovement.cs
@@ -25,21 +25,28 @@ public class PacStudentController : MonoBehaviour
             // Calculate the time based on distance and moveSpeed to achieve consistent speed.
             float moveTime = distance / moveSpeed;
 
+            // Calculate the direction to the target waypoint.
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float targetRotation = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+
             // Trigger the walking animation.
             pacStudentAnimator.SetBool("IsMoving", true);
 
-            // Tween PacStudent's position over time.
+            // Tween PacStudent's position and rotation over time.
             float elapsedTime = 0f;
             Vector3 startPosition = transform.position;
+            Quaternion startRotation = transform.rotation;
             while (elapsedTime < moveTime)
             {
                 transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / moveTime);
+                transform.rotation = Quaternion.Lerp(startRotation, Quaternion.Euler(0f, 0f, targetRotation), elapsedTime / moveTime);
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
 
-            // Ensure PacStudent reaches the exact position.
+            // Ensure PacStudent reaches the exact position and rotation.
             transform.position = targetPosition;
+            transform.rotation = Quaternion.Euler(0f, 0f, targetRotation);
 
             // Trigger the idle animation.
             pacStudentAnimator.SetBool("IsMoving", false);
